a36f24840b5cf2cae3dd254aa67b5e421668885a
Kunal Mathur <kunal.levitate2024@gmail.com>
2026-03-11 15:38:37 +0530

    TRY 5

--- stock-ui/vite.config.js
+++ stock-ui/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/health': 'http://localhost:8000',
+      '/predict': 'http://localhost:8000',
+      '/chart': 'http://localhost:8000',
+      '/portfolio': 'http://localhost:8000',
+      '/news': 'http://localhost:8000',
+      '/fundamentals': 'http://localhost:8000',
+    },
+  },
 })
